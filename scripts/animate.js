811ad7d22caa6bfb8da4094239bc589fad9a7543
xulei <xl88601131@outlook.com>
2016-10-31 14:31:21 +0800

    blog beta0.2.0

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,8 +1,7 @@
-$(function(){
 	// 返回顶部按钮
 	$(window).scroll(function(){
 		var top = $(document).scrollTop();
-		if(top > 100){
+		if(top > 180){
 			$("#totop").fadeIn(500);
 		}
 		else{
@@ -15,57 +14,102 @@ $(function(){
 			scrollTop : 0
 		},500);
 	});
-
-	//雪花效果 有bug;
-	// function snowbegin(x,y,z){
-	// 	$.fn.snow({ 			
-	// 		minSize: x,		
-	// 		maxSize: y, 	
-	// 		newOn: z	
-	// 	});
-	// }
 	
-	var timer = null;	
-	var a = 0;
-	var bodycolor = $("body").css('backgroundColor');
-	var logocolor = $(".main_header .nav_left h1 a").css('color');
-	var headercolor = $(".main_header").css('backgroundColor');
-	function onoff(ele,num){
-		$('.right_two .btn').eq(ele).on('click',function(){
-			if(num == 0){
-				$(this).css('backgroundColor',"#41F553")
-					   .children('button').css("left",15);
-				num = 1;
-				if(ele == 0){
-					$("body").css('backgroundColor','#2C2A2A');
-					$(".main_header").css('backgroundColor','#3C3A3A');
+	//给右侧第二个列表添加按钮点击变换效果
+	(function turn (){
+		var timer = null;	
+		var a = 0;
+		var bodycolor = $("body").css('backgroundColor');
+		var logocolor = $(".main_header .nav_left h1 a").css('color');
+		var headercolor = $(".main_header").css('backgroundColor');
+		function onoff(ele,num){
+			$('.right_two .btn').eq(ele).on('click',function(){
+				var that = $(this);
+				if(num == 0){
+					$(this).css('backgroundColor',"#41F553")
+						   .children('button').css("left",15);
+					num = 1;
+					if(ele == 0){
+						$("body").css('backgroundColor','#2C2A2A');
+						$(".main_header").css('backgroundColor','#3C3A3A');
+					}
+					else if(ele == 1){
+						$(".music").slideDown(300);	
+					}
 				}
-				else if(ele == 1){
-						
-				}
-				else if(ele == 2){
-						
-				}
-			}
-			else {
-				$(this).css('backgroundColor',"white")
-					   .children('button').css("left",0);
-				num = 0;
-				if(ele == 0){
-					$("body").css('backgroundColor',bodycolor);
-					$(".main_header").css('backgroundColor',headercolor);
-				}
-				else if(ele == 1){
-						
-				}
-				else if(ele == 2){
-						
-				}
-			}	
-		})
-	}
 
-	onoff(0,0);
-	onoff(1,0);
-	onoff(2,0);	
-});
+				else {
+					$(this).css('backgroundColor',"white")
+						   .children('button').css("left",0);
+					num = 0;
+					if(ele == 0){
+						$("body").css('backgroundColor',bodycolor);
+						$(".main_header").css('backgroundColor',headercolor);
+					}
+					else if(ele == 1){
+						$(".music").slideUp(300);	
+					}
+				}	
+			})
+		}	
+			for(var m=0; m<2; m++){
+				onoff(m,0);
+			}
+	})();
+
+	// (function playmusic(){
+	// 	var audio =  document.getElementsByTagName('audio');
+	// 	$.each($(".music .btn"),function(index,ele){
+	// 		$(".music .btn").eq(index).on('click',function(){
+	// 			for(var j=0; j<$(".music .btn").length; j++){
+	// 				$(".music .btn").eq(j).css('backgroundColor',"white")
+	// 					   .children('button').css("left",0);
+	// 				audio[j].pause();   
+	// 			}
+	// 			$(this).css('backgroundColor',"#41F553")
+	// 				   .children('button').css("left",15);				
+	// 			audio[index].play();	   
+	// 		});
+	// 	});
+	// })()
+
+	// var lisnum = $(".main_left ul li");
+	// // alert(lisnum.length);
+	// $(".main_right .right_one ul span").eq(0).text($(".main_left li").length);
+	// // alert($(".main_left ul li").length);
+
+
+	// var mainli = $(".main_center .main_left ul li");
+	// mainli.each(function(i){
+	// 	$(this).on('click',function(){
+	// 		// alert(i);
+	// 		$.ajax({
+	// 			type:"POST",
+	// 			url:"data/blog.json",
+	// 			dataType:"json",
+	// 			success:function(data){
+	// 				$("section").html(" ").removeClass();
+	// 				$("title").text(data.myblog[i].title);
+	// 				$(".main_header .nav_center .nav_right ul li a").eq(0).removeClass();
+	// 				var header = $("<div class='header'><h1>"+data.myblog[i].head+"</h1><h3>"+data.myblog[i].time+"</h3></div>");
+	// 				var content = $("<div class='content'></div>");
+	// 				$.each(data.myblog[i].info,function(index,ele){
+	// 					var chunk = $("<div class='chunk'><h1>"+data.myblog[i].info[index].smhead+"</h1><p>"+data.myblog[i].info[index].sminfo+"</p></div>");
+	// 					chunk.appendTo(content);
+	// 				});
+
+	// 				var divimg = $("<div class='img'></div>");
+	// 				var img = $("<img>");
+	// 				img.attr("src",data.myblog[i].img[0].src);
+	// 				img.appendTo(divimg);
+	// 				divimg.appendTo(content);
+	// 				header.appendTo($("section"));
+	// 				content.appendTo($("section"));					
+	// 			}
+	// 		})
+	// 	})
+	// });
+
+	// $(".music .btn").on('click',function(){
+	// 	console.log(111);
+	// })
---
+++ scripts/mess.js
@@ -0,0 +1,121 @@
+$(function(){
+	$.ajax({
+		type:'POST',
+		url:"data/main.json",
+		dataType:'json',		
+		success:function(data){	
+			// 给主页面添加li		
+			$.each(data.myindex,function(i){
+				var time = data.myindex[i].time;
+				var head = data.myindex[i].head;
+				var say = data.myindex[i].tosay;
+				var li = $("<li></li>");
+				var a = $("<a href='javascript:'></a>");
+				var time = $("<span class='time'>"+time+"</span>");
+				var head = $("<h2>"+head+"<h2>");
+				var say = $("<p>"+say+"</p>");
+				time.appendTo(a);
+				head.appendTo(a);
+				say.appendTo(a);
+				a.appendTo(li);
+				li.appendTo($(".main_center .main_left ul"));
+			});
+
+			//获取li的数量显示在右侧多有文章后
+			$(".main_right .right_one ul span").eq(0).text($(".main_left li").length);	
+			//给每个li绑定click方法
+			var mainli = $(".main_center .main_left ul li");
+			mainli.each(function(i){
+				$(this).on('click',function(){
+					$.ajax({
+						type:"POST",
+						url:"data/blog.json",
+						dataType:"json",
+						success:function(data){
+							$("section").html(" ").removeClass();
+							$("title").text(data.myblog[i].title);
+							$(".main_header .nav_center .nav_right ul li a").eq(0).removeClass();
+							var header = $("<div class='header'><h1>"+data.myblog[i].head+"</h1><h3>"+data.myblog[i].time+"</h3></div>");
+							var content = $("<div class='content'></div>");
+							$.each(data.myblog[i].info,function(index,ele){
+								var chunk = $("<div class='chunk'><h1>"+data.myblog[i].info[index].smhead+"</h1><p>"+data.myblog[i].info[index].sminfo+"</p></div>");
+								chunk.appendTo(content);
+							});
+
+							var divimg = $("<div class='img'></div>");
+							var img = $("<img>");
+							img.attr("src",data.myblog[i].img[0].src);
+							img.appendTo(divimg);
+							divimg.appendTo(content);
+							header.appendTo($("section"));
+							content.appendTo($("section"));					
+						},
+						error:function(){
+							console.log("数据获取失败")
+						}
+					})
+				})
+			});
+		},
+		error:function(){
+			alert('获取数据失败');
+		}
+	});
+
+	$.ajax({
+		type:'POST',
+		url:"data/music.json",
+		dataType:"json",
+		success:function(data){
+			//异步加载音乐列表
+			$.each(data.mymusic,function(i){
+				var lis = $("<li class='music'></li>");
+				lis.text(data.mymusic[i].name);
+				var onoff = $(
+						"<span class='btn'><button></button><em></em></span>"
+					);
+				var musicurl = $("<audio class='audio' style='display:none'></audio>");
+				musicurl.attr("src",data.mymusic[i].link);
+				// data.mymusic[i].name.appendTo(lis);
+				onoff.appendTo(lis);
+				musicurl.appendTo(lis);
+				lis.appendTo($(".right_two ul"));
+			});
+
+			//给加载的音乐列表添加dom操作
+			(function playmusic(){
+				var audio =  document.getElementsByTagName('audio');
+				// var num = 0;
+				$.each($(".music .btn"),function(index,ele){
+					$(this).attr('num',0);
+					$(".music .btn").eq(index).on('click',function(){
+						if($(this).attr('num') == 0 ){
+							for(var j=0; j<$(".music .btn").length; j++){
+								$(".music .btn").eq(j).css('backgroundColor',"white").attr('num',0)
+								.children('button').css("left",0);
+								audio[j].pause(); 
+							}
+							$(this).css('backgroundColor',"#41F553")
+							.children('button').css("left",15);	
+							audio[index].play();
+							$(this).attr('num',1);
+						}
+						else {
+							$(this).css('backgroundColor',"white")
+							.children('button').css("left",0);
+							audio[index].pause();	
+							$(this).attr('num',0);   
+						}	   
+					});
+				});
+			})()
+		},
+		error:function(){
+			console.log("数据读取错误");
+		}
+	});
+});
+
+
+
+	
